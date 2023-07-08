89b26dcfa6dfe58a263bc9b0968eb92e592b8d2a
Rodrigo Lusa <rodrigolusa.17@gmail.com>
2023-07-08 11:45:58 -0300

    construção do tipo lexico

---
+++ etapa3/tipolexico.c
@@ -0,0 +1,14 @@
+#include "tipolexico.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+TipoLexico* criarTipoLexico(int linha, int tipo, const char* valor) {
+    TipoLexico* novo_tipo_lexico = (TipoLexico*) malloc(sizeof(TipoLexico));
+
+    novo_tipo_lexico->linha = linha;
+    novo_tipo_lexico->tipo = tipo;
+    strncpy(novo_tipo_lexico->valor, valor, TAMANHO_MAXIMO);
+
+    return novo_tipo_lexico;
+}
