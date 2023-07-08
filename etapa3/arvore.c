46d51416cc05d293fcc5dcae71a05ba7bfcc7118
Rodrigo Lusa <rodrigolusa.17@gmail.com>
2023-07-08 13:36:53 -0300

    adição de filho e prints

--- etapa3/arvore.c
+++ etapa3/arvore.c
@@ -3,8 +3,9 @@
 #include <string.h>
 #include "arvore.h"
 
-No* criarNoTipoLexico(TipoLexico* valor_lexico) {
-    No* novo_nodo = (No*) malloc(sizeof(No));
+No *criarNoTipoLexico(TipoLexico *valor_lexico)
+{
+    No *novo_nodo = (No *)malloc(sizeof(No));
 
     novo_nodo->valor_lexico = valor_lexico;
     novo_nodo->n_filhos = 0;
@@ -14,8 +15,9 @@ No* criarNoTipoLexico(TipoLexico* valor_lexico) {
     return novo_nodo;
 }
 
-No* criarNo(char* valor) {
-    No* novo_nodo = (No*) malloc(sizeof(No));
+No *criarNo(char *valor)
+{
+    No *novo_nodo = (No *)malloc(sizeof(No));
 
     novo_nodo->valor_lexico = NULL;
     novo_nodo->n_filhos = 0;
@@ -23,4 +25,41 @@ No* criarNo(char* valor) {
     strcpy(novo_nodo->valor, valor);
 
     return novo_nodo;
-}
+
+    void adicionarFilho(No * pai, No * filho)
+    {
+        pai->filhos = (No **)realloc(pai->filhos, (pai->n_filhos + 1) * sizeof(No *));
+        pai->filhos[pai->n_filhos] = filho;
+        pai->n_filhos++;
+    }
+
+    void imprimirArestas(No * pai)
+    {
+        for (int i = 0; i < pai->n_filhos; i++)
+        {
+            printf("%p, %p\n", pai, pai->filhos[i]);
+        }
+
+        for (int i = 0; i < pai->n_filhos; i++)
+        {
+            imprimirArestas(pai->filhos[i]);
+        }
+    }
+
+    void imprimirNos(No * pai)
+    {
+        printf("%p [label=\"%s\"];\n", pai, pai->rotulo);
+
+        for (int i = 0; i < pai->n_filhos; i++)
+        {
+            imprimirNos(pai->filhos[i]);
+        }
+    }
+
+    void imprimirFilhos(No * pai)
+    {
+        for (int i = 0; i < pai->n_filhos; i++)
+        {
+            imprimirFilhos(pai->filhos[i]);
+        }
+    }
--- etapa3/arvore.h
+++ etapa3/arvore.h
@@ -1,11 +1,16 @@
 #include "tipolexico.h"
 
-typedef struct astNo {
+typedef struct astNo
+{
     char valor[TAMANHO_MAXIMO];
-    TipoLexico* valor_lexico;
+    TipoLexico *valor_lexico;
     int n_filhos;
-    struct astNo** filhos;
+    struct astNo **filhos;
 } No;
 
-No* criarNoTipoLexico(TipoLexico* valor_lexico);
-No* criarNo(char* valor);
+No *criarNoTipoLexico(TipoLexico *valor_lexico);
+No *criarNo(char *valor);
+void adicionarFilho(No *pai, No *filho);
+void imprimirArestas(No *pai);
+void imprimirNos(No *pai);
+void imprimirFilhos(No *pai);
